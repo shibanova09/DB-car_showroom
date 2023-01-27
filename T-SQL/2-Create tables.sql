2cf45e960b4b6aa6cfe7f92391ddb0d026e7af41
Alena Shibanova <81482704+shibanova09@users.noreply.github.com>
2023-01-27 21:10:12 +0300

    Update 2-Create tables.sql

--- T-SQL/2-Create tables.sql
+++ T-SQL/2-Create tables.sql
@@ -1,70 +1,70 @@
 USE CarsRoom;
 
-CREATE TABLE [Сотрудники] (
-	[Код сотрудника] int NOT NULL,
-	[Фамилия] varchar(50) NOT NULL,
-	[Имя] varchar(50) NOT NULL,
-	[Отчество] varchar(50),
-	[Возраст] varchar(10) NOT NULL,
-	[Пол] varchar(10) NOT NULL,
-	[Адрес] varchar(50) NOT NULL,
-	[Телефон] varchar(50) NOT NULL,
-	[Паспортные данные] varchar(50) NOT NULL,
-	[Код должности] int NOT NULL)
+CREATE TABLE [РЎРѕС‚СЂСѓРґРЅРёРєРё] (
+	[РљРѕРґ СЃРѕС‚СЂСѓРґРЅРёРєР°] int NOT NULL,
+	[Р¤Р°РјРёР»РёСЏ] varchar(50) NOT NULL,
+	[РРјСЏ] varchar(50) NOT NULL,
+	[РћС‚С‡РµСЃС‚РІРѕ] varchar(50),
+	[Р’РѕР·СЂР°СЃС‚] varchar(10) NOT NULL,
+	[РџРѕР»] varchar(10) NOT NULL,
+	[РђРґСЂРµСЃ] varchar(50) NOT NULL,
+	[РўРµР»РµС„РѕРЅ] varchar(50) NOT NULL,
+	[РџР°СЃРїРѕСЂС‚РЅС‹Рµ РґР°РЅРЅС‹Рµ] varchar(50) NOT NULL,
+	[РљРѕРґ РґРѕР»Р¶РЅРѕСЃС‚Рё] int NOT NULL)
 
-CREATE TABLE [Должности] (
-	[Код должности] int NOT NULL,
-	[Наименование должности] varchar(50) NOT NULL,
-	[Оклад] money NOT NULL,
-	[Обязанности] varchar(max) NOT NULL,
-	[Требования] varchar(max) NOT NULL)
+CREATE TABLE [Р”РѕР»Р¶РЅРѕСЃС‚Рё] (
+	[РљРѕРґ РґРѕР»Р¶РЅРѕСЃС‚Рё] int NOT NULL,
+	[РќР°РёРјРµРЅРѕРІР°РЅРёРµ РґРѕР»Р¶РЅРѕСЃС‚Рё] varchar(50) NOT NULL,
+	[РћРєР»Р°Рґ] money NOT NULL,
+	[РћР±СЏР·Р°РЅРЅРѕСЃС‚Рё] varchar(max) NOT NULL,
+	[РўСЂРµР±РѕРІР°РЅРёСЏ] varchar(max) NOT NULL)
 
-CREATE TABLE [Производители] (
-	[Код производителя] int NOT NULL,
-	[Наименование] varchar(50) NOT NULL,
-	[Страна] varchar(50) NOT NULL,
-	[Адрес] varchar(50) NOT NULL,
-	[Описание] varchar(max) NOT NULL,
-	[Код сотрудника] int NOT NULL)
+CREATE TABLE [РџСЂРѕРёР·РІРѕРґРёС‚РµР»Рё] (
+	[РљРѕРґ РїСЂРѕРёР·РІРѕРґРёС‚РµР»СЏ] int NOT NULL,
+	[РќР°РёРјРµРЅРѕРІР°РЅРёРµ] varchar(50) NOT NULL,
+	[РЎС‚СЂР°РЅР°] varchar(50) NOT NULL,
+	[РђРґСЂРµСЃ] varchar(50) NOT NULL,
+	[РћРїРёСЃР°РЅРёРµ] varchar(max) NOT NULL,
+	[РљРѕРґ СЃРѕС‚СЂСѓРґРЅРёРєР°] int NOT NULL)
 
-CREATE TABLE [Дополнительное оборудование] (
-	[Код оборудования] int NOT NULL,
-	[Наименование] varchar(50) NOT NULL,
-	[Характеристики] varchar(max) NOT NULL,
-	[Цена] money NOT NULL)
+CREATE TABLE [Р”РѕРїРѕР»РЅРёС‚РµР»СЊРЅРѕРµ РѕР±РѕСЂСѓРґРѕРІР°РЅРёРµ] (
+	[РљРѕРґ РѕР±РѕСЂСѓРґРѕРІР°РЅРёСЏ] int NOT NULL,
+	[РќР°РёРјРµРЅРѕРІР°РЅРёРµ] varchar(50) NOT NULL,
+	[РҐР°СЂР°РєС‚РµСЂРёСЃС‚РёРєРё] varchar(max) NOT NULL,
+	[Р¦РµРЅР°] money NOT NULL)
 
-CREATE TABLE [Тип кузова] (
-	[Код типа кузова] int NOT NULL,
-	[Название] varchar(50) NOT NULL,
-	[Описание] varchar(max) NOT NULL)
+CREATE TABLE [РўРёРї РєСѓР·РѕРІР°] (
+	[РљРѕРґ С‚РёРїР° РєСѓР·РѕРІР°] int NOT NULL,
+	[РќР°Р·РІР°РЅРёРµ] varchar(50) NOT NULL,
+	[РћРїРёСЃР°РЅРёРµ] varchar(max) NOT NULL)
 
-CREATE TABLE [Автомобили] (
-	[Код автомобиля] int NOT NULL,
-	[Марка] varchar(50) NOT NULL,
-	[Код производителя] int NOT NULL,
-	[Код типа кузова] int NOT NULL,
-	[Дата производства] date NOT NULL,
-	[Цвет] varchar(50) NOT NULL,
-	[Номер кузова] bigint NOT NULL,
-	[Номер двигателя] bigint NOT NULL,
-	[Характеристики] varchar(max) NOT NULL,
-	[Код оборудования 1] int NOT NULL,
-	[Код оборудования 2] int NOT NULL,
-	[Код оборудования 3] int NOT NULL,
-	[Цена] money NOT NULL,
-	[Код сотрудника] int NOT NULL)
+CREATE TABLE [РђРІС‚РѕРјРѕР±РёР»Рё] (
+	[РљРѕРґ Р°РІС‚РѕРјРѕР±РёР»СЏ] int NOT NULL,
+	[РњР°СЂРєР°] varchar(50) NOT NULL,
+	[РљРѕРґ РїСЂРѕРёР·РІРѕРґРёС‚РµР»СЏ] int NOT NULL,
+	[РљРѕРґ С‚РёРїР° РєСѓР·РѕРІР°] int NOT NULL,
+	[Р”Р°С‚Р° РїСЂРѕРёР·РІРѕРґСЃС‚РІР°] date NOT NULL,
+	[Р¦РІРµС‚] varchar(50) NOT NULL,
+	[РќРѕРјРµСЂ РєСѓР·РѕРІР°] bigint NOT NULL,
+	[РќРѕРјРµСЂ РґРІРёРіР°С‚РµР»СЏ] bigint NOT NULL,
+	[РҐР°СЂР°РєС‚РµСЂРёСЃС‚РёРєРё] varchar(max) NOT NULL,
+	[РљРѕРґ РѕР±РѕСЂСѓРґРѕРІР°РЅРёСЏ 1] int NOT NULL,
+	[РљРѕРґ РѕР±РѕСЂСѓРґРѕРІР°РЅРёСЏ 2] int NOT NULL,
+	[РљРѕРґ РѕР±РѕСЂСѓРґРѕРІР°РЅРёСЏ 3] int NOT NULL,
+	[Р¦РµРЅР°] money NOT NULL,
+	[РљРѕРґ СЃРѕС‚СЂСѓРґРЅРёРєР°] int NOT NULL)
 
-CREATE TABLE [Заказчики] (
-	[Фамилия] varchar(50) NOT NULL,
-	[Имя] varchar(50) NOT NULL,
-	[Отчество] varchar(50),
-	[Адрес] varchar(50) NOT NULL,
-	[Телефон] varchar(50) NOT NULL,
-	[Паспортные данные] varchar(50) NOT NULL,
-	[Код автомобиля] int NOT NULL,
-	[Дата заказа] date NOT NULL,
-	[Дата продажи] date NOT NULL,
-	[Отметка о выполнении] bit NOT NULL,
-	[Отметка об оплате] bit NOT NULL,
-	[Процент предоплаты] decimal(5,2) NOT NULL,
-	[Код сотрудника] int NOT NULL)
+CREATE TABLE [Р—Р°РєР°Р·С‡РёРєРё] (
+	[Р¤Р°РјРёР»РёСЏ] varchar(50) NOT NULL,
+	[РРјСЏ] varchar(50) NOT NULL,
+	[РћС‚С‡РµСЃС‚РІРѕ] varchar(50),
+	[РђРґСЂРµСЃ] varchar(50) NOT NULL,
+	[РўРµР»РµС„РѕРЅ] varchar(50) NOT NULL,
+	[РџР°СЃРїРѕСЂС‚РЅС‹Рµ РґР°РЅРЅС‹Рµ] varchar(50) NOT NULL,
+	[РљРѕРґ Р°РІС‚РѕРјРѕР±РёР»СЏ] int NOT NULL,
+	[Р”Р°С‚Р° Р·Р°РєР°Р·Р°] date NOT NULL,
+	[Р”Р°С‚Р° РїСЂРѕРґР°Р¶Рё] date NOT NULL,
+	[РћС‚РјРµС‚РєР° Рѕ РІС‹РїРѕР»РЅРµРЅРёРё] bit NOT NULL,
+	[РћС‚РјРµС‚РєР° РѕР± РѕРїР»Р°С‚Рµ] bit NOT NULL,
+	[РџСЂРѕС†РµРЅС‚ РїСЂРµРґРѕРїР»Р°С‚С‹] decimal(5,2) NOT NULL,
+	[РљРѕРґ СЃРѕС‚СЂСѓРґРЅРёРєР°] int NOT NULL)
